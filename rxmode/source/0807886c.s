d2ad18447d270423cadb407d0aa03a4d9362d9da
dukesrg <dukesrg@gmail.com>
2015-06-26 15:52:07 +0300

    p9 emunand hook patch ported to AS

---
+++ rxmode/source/0807886c.s
@@ -0,0 +1,12 @@
+// ---------------------------------------- //
+// |      Copyright(c) 2015, Roxas75      | //
+// |         All rights reserved.         | //
+// ---------------------------------------- //
+@ p9 emunand hook
+.thumb
+.align 2
+
+	ldr r4, =0x0801A5B0
+	bx r4
+
+.pool
