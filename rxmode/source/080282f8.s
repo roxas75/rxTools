b0287b8fd4da6b34b2e551919d3dab4d0479c367
dukesrg <dukesrg@gmail.com>
2015-06-26 16:33:59 +0300

    fixed jump offsets with explicit values

--- rxmode/source/080282f8.s
+++ rxmode/source/080282f8.s
@@ -10,8 +10,8 @@
 	ldr 	r2, =0x801A6D0 		//Here we'll put the keyX, this is a redirection
 	mov	    r1, #5
 	mov 	r0, #0x25
-	bl		0x080575B4-load_offset
-	bl		0x080575B4-load_offset
+	.hword 0xF02F, 0xF959	@bl		0x080575B4-load_offset
+	.hword 0xF037, 0xFC21	@bl		0x0805FB48-load_offset
 	pop 	{r4-r6,pc}
 
 .pool
