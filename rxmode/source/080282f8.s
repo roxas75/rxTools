a777044e4ef06a7cd684fa0d3202a9bd7bec22a1
dukesrg <dukesrg@gmail.com>
2015-06-26 15:43:24 +0300

    p9 keyx spoo ported to AS, offsets need to be fixed!

---
+++ rxmode/source/080282f8.s
@@ -0,0 +1,17 @@
+// ---------------------------------------- //
+// |      Copyright(c) 2015, Roxas75      | //
+// |         All rights reserved.         | //
+// ---------------------------------------- //
+@ p9 keyx spoof
+.equ load_offset, 0x080282F8
+.thumb
+.align 2
+
+	ldr 	r2, =0x801A6D0 		//Here we'll put the keyX, this is a redirection
+	mov	    r1, #5
+	mov 	r0, #0x25
+	bl		0x080575B4-load_offset
+	bl		0x080575B4-load_offset
+	pop 	{r4-r6,pc}
+
+.pool
