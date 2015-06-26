86cbc6b896f895002058521f44480dd8e2002ee5
dukesrg <dukesrg@gmail.com>
2015-06-26 15:44:53 +0300

    p9 signature spoof patch ported to AS, offset needs to be fixed!

---
+++ rxmode/source/080632b8.s
@@ -0,0 +1,11 @@
+// ---------------------------------------- //
+// |      Copyright(c) 2015, Roxas75      | //
+// |         All rights reserved.         | //
+// ---------------------------------------- //
+@ p9 signature spoof
+.equ load_offset, 0x080632B8
+.thumb
+.align 1
+	mov		r0, #0
+	b		0x80631AA-load_offset
+.pool
