b41b95fd5e5db0fccb17788e6b1ecedfe11c0ec7
luxsie <luxsie@163.com>
2015-06-28 23:06:10 +0800

    Re-Arranged the FS functions, formatted as Java style with Tab indention.

--- rxtools/source/lib/fs.h
+++ rxtools/source/lib/fs.h
@@ -4,30 +4,31 @@
 #include "ff.h"
 #define File FIL
 
-enum{
+enum {
 	TWLN = 0x00012E00,
 	TWLP = 0x09011A00,
 	AGB_SAVE = 0x0B100000,
 	FIRM0 = 0x0B130000,
 	FIRM1 = 0x0B530000,
 	CTRNAND = 0x0B95CA00,
-}NAND_PARTITIONS;
-
+} NAND_PARTITIONS;
+////////////////////////////////////////////////////////////////Basic FileSystem Operations
 bool InitFS();
 void DeinitFS();
-
-bool FileOpen(File* Handle, const char* path, bool truncate);
-size_t FileRead(File* Handle, void* buf, size_t size, size_t foffset);
-size_t FileWrite(File* Handle, void* buf, size_t size, size_t foffset);
-size_t FileGetSize(File* Handle);
-void FileClose(File* Handle);
-int FileCopy(char* dest, char* source);
-u32 FSFileCopy(char* target, char* source);
-
+bool FileOpen(File *Handle, const char *path, bool truncate);
+size_t FileRead(File *Handle, void *buf, size_t size, size_t foffset);
+size_t FileWrite(File *Handle, void *buf, size_t size, size_t foffset);
+size_t FileGetSize(File *Handle);
+void FileClose(File *Handle);
+////////////////////////////////////////////////////////////////Advanced FileSystem Operations
+int FileCopy(char *dest, char *source);
+u32 FSFileCopy(char *target, char *source);
+////////////////////////////////////////////////////////////////NAND Operations
 int GetSystemVersion();
+void FSNandInitCrypto(void);
 int checkEmuNAND();
-void GetNANDCTR(u8* ctr);
-int nand_readsectors(uint32_t sector_no, uint32_t numsectors, uint8_t *out, unsigned int partition);
-int nand_writesectors(uint32_t sector_no, uint32_t numsectors, uint8_t *out, unsigned int partition);
-int emunand_readsectors(uint32_t sector_no, uint32_t numsectors, uint8_t *out, unsigned int partition);
-int emunand_writesectors(uint32_t sector_no, uint32_t numsectors, uint8_t *out, unsigned int partition);
+void GetNANDCTR(u8 *ctr);
+void nand_readsectors(uint32_t sector_no, uint32_t numsectors, uint8_t *out, unsigned int partition);
+void nand_writesectors(uint32_t sector_no, uint32_t numsectors, uint8_t *out, unsigned int partition);
+void emunand_readsectors(uint32_t sector_no, uint32_t numsectors, uint8_t *out, unsigned int partition);
+void emunand_writesectors(uint32_t sector_no, uint32_t numsectors, uint8_t *out, unsigned int partition);
