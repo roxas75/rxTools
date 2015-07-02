a7f1eff0fd1004a20aee7fbff04db3320b190fe6
dukesrg <dukesrg@gmail.com>
2015-07-02 16:48:38 +0300

    Fixed indents, added FONT_SIZE Get/DrawPixel pointer type fixed, functions parameters switched to uxx Switched color format to framebuffer compatible Transparency changed

--- rxtools/source/main.c
+++ rxtools/source/main.c
@@ -14,18 +14,18 @@
 #include "configuration.h"
 
 void Initialize(){
-	DrawString(TOP_SCREEN,  " INITIALIZE... ", 0, 240-8, WHITE, BLACK);
+	DrawString(TOP_SCREEN,  " INITIALIZE... ", 0, SCREEN_HEIGHT-FONT_SIZE, WHITE, BLACK);
 	if(FSInit()){
-		DrawString(TOP_SCREEN,  " LOADING...    ", 0, 240-8, WHITE, BLACK);
+		DrawString(TOP_SCREEN,  " LOADING...    ", 0, SCREEN_HEIGHT-FONT_SIZE, WHITE, BLACK);
 	}else{
-		DrawString(TOP_SCREEN,  " ERROR!        ", 0, 240-8, RED, BLACK);
+		DrawString(TOP_SCREEN,  " ERROR!        ", 0, SCREEN_HEIGHT-FONT_SIZE, RED, BLACK);
 	}
 	LoadPack();
 	//Console Stuff
-	memset(TOP_SCREEN, 0x00, 0x46500);
-	memset(TOP_SCREEN2, 0x00, 0x46500);
+	memset(TOP_SCREEN, 0x00, SCREEN_SIZE);
+	memset(TOP_SCREEN2, 0x00, SCREEN_SIZE);
 	ConsoleSetXY(15, 15);
-	ConsoleSetWH(370, 160);
+	ConsoleSetWH(SCREEN_WIDTH-30, SCREEN_HEIGHT-80);
 	ConsoleSetBorderColor(BLUE);
 	ConsoleSetTextColor(WHITE);
 	ConsoleSetBackgroundColor(BLACK);
@@ -42,15 +42,15 @@ void Initialize(){
 		u32 pad = GetInput();
 		if(pad & BUTTON_R1 && i > 0x333333) goto rxTools_boot;
 	}
-    rxModeQuickBoot();
+	rxModeQuickBoot();
 	rxTools_boot:
-	memset(TOP_SCREEN, 0x00, 0x46500);
-	memset(TOP_SCREEN2, 0x00, 0x46500);
+	memset(TOP_SCREEN, 0x00, SCREEN_SIZE);
+	memset(TOP_SCREEN2, 0x00, SCREEN_SIZE);
 }
 
 int main(){
 	Initialize();
-	DrawString(TOP_SCREEN, "SUPPORT THE ORIGINAL, NOT THE IMITATION!", 75, 240-10, GREY, BLACK);
+	DrawString(TOP_SCREEN, "SUPPORT THE ORIGINAL, NOT THE IMITATION!", 75, SCREEN_HEIGHT-10, GREY, BLACK);
 	//7.X Keys stuff
 	File KeyFile;
 	if(FileOpen(&KeyFile, "/slot0x25KeyX.bin", 0)){
@@ -58,7 +58,7 @@ int main(){
 		FileRead(&KeyFile, keyX, 16, 0);
 		FileClose(&KeyFile);
 		setup_aeskeyX(0x25, keyX);
-		DrawString(TOP_SCREEN, " NewKeyX ", 0, 240-8, GREEN, BLACK);
+		DrawString(TOP_SCREEN, " NewKeyX ", 0, SCREEN_HEIGHT-FONT_SIZE, GREEN, BLACK);
 	}else{
 		if(GetSystemVersion() < 3){
 			ConsoleInit();
@@ -66,18 +66,18 @@ int main(){
 			ConsoleShow();
 			WaitForButton(BUTTON_A);
 		}
-		DrawString(TOP_SCREEN, " NewKeyX ", 0, 240-8, RED, BLACK);
+		DrawString(TOP_SCREEN, " NewKeyX ", 0, SCREEN_HEIGHT-FONT_SIZE, RED, BLACK);
 	}
-	DrawString(TOP_SCREEN, " EmuNAND ", 0, 240-16, checkEmuNAND() ? GREEN : RED, BLACK);
+	DrawString(TOP_SCREEN, " EmuNAND ", 0, SCREEN_HEIGHT-FONT_SIZE*2, checkEmuNAND() ? GREEN : RED, BLACK);
 
 	//That's the Main Menu initialization, easy and cool
 	MenuInit(&MainMenu);
 	MenuShow();
 
-    while (true) {
-		DrawString(TOP_SCREEN,  "[SELECT] Reboot", 349-18*8, 181-24-8, RED, BLACK);
-		DrawString(TOP_SCREEN,  "[START]  Shutdown", 349-18*8, 181-24, RED, BLACK);
-        u32 pad_state = InputWait();
+	while (true) {
+		DrawString(TOP_SCREEN,  "[SELECT] Reboot", SCREEN_WIDTH-51-18*FONT_SIZE, SCREEN_HEIGHT-59-24-FONT_SIZE, RED, BLACK);
+		DrawString(TOP_SCREEN,  "[START]  Shutdown", SCREEN_WIDTH-51-18*FONT_SIZE, SCREEN_HEIGHT-59-24, RED, BLACK);
+		u32 pad_state = InputWait();
 		if(pad_state & BUTTON_DOWN) 	MenuNextSelection();
 		if(pad_state & BUTTON_UP)   	MenuPrevSelection();
 		if(pad_state & BUTTON_A)    	MenuSelect();
@@ -85,8 +85,8 @@ int main(){
 		if(pad_state & BUTTON_START)	ShutDown();
 		TryScreenShot();
 		MenuShow();
-    }
+	}
 
-    FSDeInit();
-    return 0;
+	FSDeInit();
+	return 0;
 }
