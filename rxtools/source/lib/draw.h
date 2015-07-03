59633fe76568db52974833bd62edb7311413163d
AlbertoSONIC <albertosonic9000@gmail.com>
2015-07-03 15:52:45 +0200

    Gui work in progress - still many bugs/unfinished things

--- rxtools/source/lib/draw.h
+++ rxtools/source/lib/draw.h
@@ -3,13 +3,14 @@
 #include "common.h"
 //Screen Macros
 #define BYTES_PER_PIXEL	3  //Our color buffer accepts 24-bits color.
-#define SCREEN_WIDTH	400
+#define SCREEN_WIDTH	320
 #define SCREEN_HEIGHT	240
 #define SCREEN_SIZE	(BYTES_PER_PIXEL*SCREEN_WIDTH*SCREEN_HEIGHT)
 #define FONT_SIZE	8
-#define TOP_SCREEN	(u8*)(*(u32*)((u32)0x080FFFC0 + 4 * (*(u32*)0x080FFFD8 & 1)))
-#define TOP_SCREEN2	(u8*)(*(u32*)0x080FFFC4)
-#define BOT_SCREEN	(u8*)(*(u32*)0x080FFFD4)
+#define TOP_SCREEN          (u8*)(*(u32*)0x080FFFC0)
+#define TOP_SCREEN2         (u8*)(*(u32*)0x080FFFC4)
+#define BOT_SCREEN          (u8*)(*(u32*)0x080FFFD0)
+#define BOT_SCREEN2         (u8*)(*(u32*)0x080FFFD4)
 
 //Colors Macros
 #define ARGB(a,r,g,b)	(a<<24|r<<16|g<<8|b) //console asks for B,G,R in bytes
@@ -24,12 +25,15 @@
 #define GREY		RGB(0x77, 0x77, 0x77) //GW Gray shade
 
 void ClearScreen(u8 *screen, u32 color);
+void DrawClearScreenAll(void);
 void DrawCharacter(u8 *screen, char character, u32 x, u32 y, u32 color, u32 bgcolor);
 void DrawString(u8 *screen, const char *str, u32 x, u32 y, u32 color, u32 bgcolor);
 void DrawPixel(u8 *screen, u32 x, u32 y, u32 color);
 u32 GetPixel(u8 *screen, u32 x, u32 y);
 void Debug(const char *format, ...);
 
+void DrawTopSplash(char splash_file[]);
+void DrawBottomSplash(char splash_file[]);
 void SplashScreen();
 //Unused functions.
 void DrawHex(u8 *screen, u32 hex, u32 x, u32 y, u32 color, u32 bgcolor);
