1b4cbcb1a25e35ed13867d9bf1628ab38e7aaa87
Jack Davies <lavanoid@gmail.com>
2015-07-04 21:18:00 +0100

    Fixed file encoding

--- theme/makebgr.sh
+++ theme/makebgr.sh
@@ -2,8 +2,8 @@
 convert -rotate 90 $1 $1.bgr
 if [ -f ${1%.*}.bin ]
 then
-	echo Removing old file...
-	rm ${1%.*}.bin
+        echo Removing old file...
+        rm ${1%.*}.bin
 fi
 mv $1.bgr ${1%.*}.bin
 
