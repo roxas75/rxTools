8193dd91665b95eb6fc681e6543a37feabc30be5
ApacheThunder <spenserwhaley@gmail.com>
2015-07-04 16:40:40 -0500

    Latest commits
    Synced with latest commits. Also forgot to include modified filepack.c.

---
+++ theme/makebgr-all.bat
@@ -0,0 +1,26 @@
+@echo off
+REM This script was written by Lavanoid (WhoAmI?). Enjoy!
+@echo Converting...
+cd %~dp0
+set i=0
+Title = Converting images. Processed: %i%
+for %%f in (*.png) do call :convertfile "%%f"
+@echo --------------------------------------------
+@echo All files have been processed.
+@echo.
+@echo         Script written by Lavanoid (WhoAmI?)
+@echo --------------------------------------------
+@echo Press any key.
+pause >nul
+goto :end
+:convertfile
+set /a i=%i%+1
+echo File: %~nx1
+echo Bin: %~n1.bin
+if exist "%~n1.bin" @echo Removing "%~n1.bin"...
+if exist "%~n1.bin" del /f "%~n1.bin"
+if exist "%~n1.bin" @echo Failed to remove "%~n1.bin"! & goto :end
+convert -rotate 90 %~nx1 %~n1.bin
+if exist "%~n1.bin" @echo Successfully generated "%~n1.bin"!
+Title = Converting images. Processed: %i%
+:end
--- theme/makebgr.sh
+++ theme/makebgr.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
 convert -rotate 90 $1 $1.bgr
 if [ -f ${1%.*}.bin ]
 then
-	echo Removing old file...
-	rm ${1%.*}.bin
+        echo Removing old file...
+        rm ${1%.*}.bin
 fi
 mv $1.bgr ${1%.*}.bin
+
