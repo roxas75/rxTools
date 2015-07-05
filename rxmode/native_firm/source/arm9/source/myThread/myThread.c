25199e229acb9ba7dd3bacb1796a8cb7408055dd
173210 <root.3.173210@live.com>
2015-07-05 15:38:15 +0900

    myThread.c: Update variable types

--- rxmode/native_firm/source/arm9/source/myThread/myThread.c
+++ rxmode/native_firm/source/arm9/source/myThread/myThread.c
@@ -6,6 +6,7 @@
 #include "FS.h"
 #include <wchar.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #ifdef MEMDUMP
 unsigned char handle[32];
@@ -21,30 +22,30 @@ static void memdump(wchar_t* filename, unsigned char* buf, unsigned int size){
 }
 #endif
 
-static unsigned char originalcode[] = { 0x00, 0x00, 0x55, 0xE3, 0x01, 0x10, 0xA0, 0xE3, 0x11, 0x00, 0xA0, 0xE1, 0x03, 0x00, 0x00, 0x0A };
-static unsigned char patchcode[] = { 0x01, 0x00, 0xA0, 0xE3, 0x70, 0x80, 0xBD, 0xE8 };
-static char* dest = (void*)0x20000400;
+static int32_t originalcode[] = { 0xE3550000, 0xE3A01001, 0xE1A00011, 0x0A000003 };
+static int32_t patchcode[] = { 0xE3A00001, 0xE8BD8070 };
+static int32_t* dest = (void*)0x20000400;
 static void patchregion(){
-	for(int i = 0; i < 8; i++) *(dest + i) = patchcode[i];
+	for(int i = 0; i < 8 / sizeof(int32_t); i++) dest[i] = patchcode[i];
 }	
 
 static void patch_processes(){
-	char* mset = (void*)0x24000000;
-	char* menu = (void*)0x26A00000;
-	for(int i = 0; i < 0x600000; i+=4){
+	const uintptr_t mset = 0x24000000;
+	const uintptr_t menu = 0x26A00000;
+	for(int i = 0; i < 0x600000; i++){
 		//System Menu code, which locks the region
 		if(dest == (void*)0x20000400){	//This means we haven't still found our code
-			if( (*((unsigned int*)(menu + i + 0x0)) == *((unsigned int*)&originalcode[0x0])) &&
-				(*((unsigned int*)(menu + i + 0x4)) == *((unsigned int*)&originalcode[0x4])) &&
-				(*((unsigned int*)(menu + i + 0x8)) == *((unsigned int*)&originalcode[0x8])) &&
-				(*((unsigned int*)(menu + i + 0xC)) == *((unsigned int*)&originalcode[0xC]))){	
-				dest = menu + i;    //Basically, once we found where the code is, there is no point on searching it again
+			if( ((*((int32_t *)(menu + i + 0x0)) == originalcode[0]) &&
+				((*((int32_t *)(menu + i + 1))) == originalcode[1]) &&
+				((*((int32_t *)(menu + i + 2))) == originalcode[2]) &&
+				((*((int32_t *)(menu + i + 3))) == originalcode[3]))){	
+				dest = (int32_t *)(menu + i);    //Basically, once we found where the code is, there is no point on searching it again
 				break;
 			}
 		}
 		//System Settings label
-		if(rx_strcmp(mset - i, "Ver.", 4, 2, 1)){
-			rx_strcpy(mset - i, "Shit", 4, 2, 1);
+		if(rx_strcmp((char *)mset - i, "Ver.", 4, 2, 1)){
+			rx_strcpy((char *)mset - i, "Shit", 4, 2, 1);
 		}
 	}
 }
@@ -57,7 +58,7 @@ void myThread(){
 		}
 #endif
 		patch_processes();
-		if(*((unsigned int*)dest) != *((unsigned int*)&patchcode[0]))
+		if(*dest != patchcode[0])
 			svc_Backdoor(&patchregion);		//Edit just if the code is not patched, or the arm9 will get mad
 	}
 	__asm("SVC 0x09");
