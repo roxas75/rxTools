38153c342bdfce8497a46cc10722bdafafd9c13d
173210 <root.3.173210@live.com>
2015-08-05 17:15:44 +0900

    Update tools/toolsrc/font_tool/main.c

--- tools/toolsrc/font_tool/main.c
+++ tools/toolsrc/font_tool/main.c
@@ -15,56 +15,109 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#include <errno.h>
 #include <stdio.h>
 #include <malloc.h>
-#ifndef NON_UNICODE
-	#define FONT_WIDTH	16
-	#define FONT_HEIGHT	16
-	#define CHAR_COLUMNS	256
-	#define CHAR_ROWS	256
+
+#ifdef NON_UNICODE
+
+#define FONT_WIDTH	8
+#define FONT_HEIGHT	8
+#define CHAR_COLUMNS	16
+#define CHAR_ROWS	16
+
 #else
-	#define FONT_WIDTH	8
-	#define FONT_HEIGHT	8
-	#define CHAR_COLUMNS	16
-	#define CHAR_ROWS	16
+
+#define FONT_WIDTH	16
+#define FONT_HEIGHT	16
+#define CHAR_COLUMNS	256
+#define CHAR_ROWS	256
+
 #endif
-#define fontsize	CHAR_COLUMNS*CHAR_ROWS*FONT_WIDTH*FONT_HEIGHT/FONT_WIDTH
-int main(int argc, char** argv){
-	if(argc < 3){
-		printf("Usage : font_tool.exe <font(%dx%dx1bpp).bin> <font(%dx%dx1bpp).bin>\n\n"
-		       "This program is free software; you can redistribute it and/or\n"
-	         "modify it under the terms of the GNU General Public License\n"
-	         "version 2 as published by the Free Software Foundation\n",
-					 FONT_WIDTH*CHAR_COLUMNS,FONT_HEIGHT*CHAR_ROWS,FONT_WIDTH,CHAR_COLUMNS*FONT_HEIGHT*CHAR_ROWS);
+
+#if FONT_WIDTH <= 8
+#define FONT_TYPE unsigned char
+#elif FONT_WIDTH <= 16
+#define FONT_TYPE unsigned short
+#endif
+
+int main(int argc, char** argv)
+{
+	FONT_TYPE (* font)[CHAR_COLUMNS][FONT_HEIGHT];
+	FILE *fp;
+	unsigned int i, j, k, l;
+	FONT_TYPE m;
+
+	if (argc != 3) {
+		fprintf(stderr, "Usage : %s <font(%dx%dx1bpp).bin> <font(%dx%dx1bpp).bin>\n\n"
+			"This program is free software; you can redistribute it and/or\n"
+			"modify it under the terms of the GNU General Public License\n"
+			"version 2 as published by the Free Software Foundation\n",
+			argv[0],
+			FONT_WIDTH * CHAR_COLUMNS, FONT_HEIGHT * CHAR_ROWS,
+			FONT_WIDTH, CHAR_COLUMNS * FONT_HEIGHT * CHAR_ROWS);
+
 		return -1;
 	}
-#if FONT_WIDTH <= 8
-	unsigned char *font = malloc(fontsize);
-#elif FONT_WIDTH <= 16
-	unsigned short *font = malloc(fontsize*2);
-#endif
-	FILE * pFile;
-	int i,j,k,l;
-	pFile=fopen(argv[1],"rb");
-	for(k=0; k<CHAR_ROWS; k++){
-		for(j=0; j<FONT_HEIGHT; j++){
-			for(i=0; i<CHAR_COLUMNS; i++){
-				fread(&font[i*FONT_HEIGHT+j+k*FONT_HEIGHT*CHAR_COLUMNS],1,sizeof(*font),pFile);
+
+	font = malloc(CHAR_ROWS * CHAR_COLUMNS * FONT_HEIGHT * sizeof(FONT_TYPE));
+	if (font == NULL) {
+		perror(NULL);
+		return errno;
+	}
+
+	fp = fopen(argv[1], "rb");
+	if (fp == NULL) {
+		perror(argv[1]);
+		goto fail;
+	}
+
+	for (i = 0; i < CHAR_ROWS; i++)
+		for (j = 0; j < FONT_HEIGHT; j++)
+			for (k = 0; k < CHAR_COLUMNS; k++)
+				if (fread(&font[i][k][j], sizeof(FONT_TYPE), 1, fp) != 1)
+					perror(argv[1]);
+
+	if (fclose(fp))
+		perror(argv[1]);
+
+	fp = fopen(argv[2], "wb");
+	if (fp == NULL) {
+		perror(argv[2]);
+		goto fail;
+	}
+
+	for (i = 0; i < CHAR_ROWS; i++)
+		for (j = 0; j < CHAR_COLUMNS; j++) {
+			k = FONT_WIDTH;
+			while (k > 0) {
+				k--;
+
+				m = 0;
+				for (l = 0; l < FONT_WIDTH; l++)
+					m |= (((font[i][j][l] >> ((k + 8) & (FONT_WIDTH - 1))) & 1) << ((FONT_WIDTH - 1) - l));
+//					m|=(((font[i+l] >> ((k+8)&(FONT_WIDTH-1))) & 1) << ((FONT_WIDTH-1-l+8)&(FONT_WIDTH-1)));
+
+				if (fwrite(&m, sizeof(m), 1, fp) != 1) {
+					perror(argv[2]);
+
+					if (fclose(fp))
+						perror(argv[2]);
+
+					goto fail;
+				}
 			}
 		}
+
+	if (fclose(fp)) {
+		perror(argv[2]);
+		goto fail;
 	}
-	fclose(pFile);
-	pFile=fopen(argv[2],"wb");
-	for(i=0; i<fontsize; i+=FONT_WIDTH){
-		for(j=FONT_WIDTH-1; j>=0; j--){
-			for(l=0,k=0; k<FONT_WIDTH; k++){
-//				l|=(((font[i+k] >> ((j+8)&(FONT_WIDTH-1))) & 1) << ((FONT_WIDTH-1-k+8)&(FONT_WIDTH-1)));
-				l|=(((font[i+k] >> ((j+8)&(FONT_WIDTH-1))) & 1) << (FONT_WIDTH-1-k));
-			}
-			fwrite(&l,1,sizeof(*font),pFile);
-		}
-	}
-	fclose(pFile);
+
 	free(font);
 	return 0;
+
+fail:
+	free(font);
+	return errno;
 }
