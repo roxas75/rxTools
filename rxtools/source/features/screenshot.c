f3a839cbf7d2f843cd4a819973a1205544b8465a
Soar Qin <soarchin@gmail.com>
2015-08-12 11:08:42 +0800

    Improved screenshot, made it much faster, with bottom screen captured

--- rxtools/source/features/screenshot.c
+++ rxtools/source/features/screenshot.c
@@ -24,36 +24,78 @@
 #include "fatfs/ff.h"
 #include "stdio.h"
 
-unsigned char bmpTopHeader[] = {
-	0x42, 0x4D, 0x36, 0x65, 0x04, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00,
-	0x00, 0x00, 0x90, 0x01, 0x00, 0x00, 0xF0, 0x00,
-	0x00, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x65, 0x04, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-	};
-
 void ScreenShot(){
+	unsigned char bmpHeader[] = {
+		0x42, 0x4D, 0x36, 0x65, 0x04, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00,
+		0x00, 0x00, 0x90, 0x01, 0x00, 0x00, 0xF0, 0x00,
+		0x00, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x65, 0x04, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+		};
+
 	File myFile;
 	char tmp[256]; int count = 0;
+	unsigned int written = 0;
+	char* bmp_cache; char* bmp_ptr;
+	u8 (*screen_ptr) [SCREEN_HEIGHT][BYTES_PER_PIXEL];
+
 	f_mkdir ("Screenshot");
+
 	do{
 		sprintf(tmp, "/Screenshot/top_screen_%d.bmp", count++);
 	}while(FileOpen(&myFile, tmp, 0));
 
 	if(FileOpen(&myFile, tmp, 1)){
-		FileWrite(&myFile, bmpTopHeader, 0x36, 0);
-		int pos = 0x36;
-		for(int i = SCREEN_HEIGHT-1; i >= 0; i--){
-			for(int j = 0; j < BOT_SCREEN_WIDTH; j++){
-				int color = GetPixel(TOP_SCREEN, j, i);
-				FileWrite(&myFile, &color, 0x4, pos);
-				pos += 3;
+		unsigned int bmp_size = TOP_SCREEN_WIDTH*SCREEN_HEIGHT*3;
+		bmp_cache = (char*)malloc(bmp_size + 0x36);
+		bmp_ptr = bmp_cache;
+		*(unsigned int*)(bmpHeader+0x02) = bmp_size + 0x36;
+		*(unsigned int*)(bmpHeader+0x12) = TOP_SCREEN_WIDTH;
+		*(unsigned int*)(bmpHeader+0x16) = SCREEN_HEIGHT;
+		*(unsigned int*)(bmpHeader+0x22) = bmp_size;
+		f_write(&myFile, bmpHeader, 0x36, &written);
+		screen_ptr = (void*)TOP_SCREEN;
+		for(int y = 0; y < SCREEN_HEIGHT; y++){
+			for(int x = 0; x < TOP_SCREEN_WIDTH; x++){
+				memcpy(bmp_ptr, screen_ptr[x][y], 3);
+				bmp_ptr += 3;
 			}
 		}
+		f_write(&myFile, bmp_cache, bmp_size + 0x36, &written);
+		f_sync(&myFile);
+		free(bmp_cache);
 		FileClose(&myFile);
 	}
+
+	count = 0;
+	do{
+		sprintf(tmp, "/Screenshot/bot_screen_%d.bmp", count++);
+	}while(FileOpen(&myFile, tmp, 0));
+
+	if(FileOpen(&myFile, tmp, 1)){
+		unsigned int bmp_size = BOT_SCREEN_WIDTH*SCREEN_HEIGHT*3;
+		bmp_cache = (char*)malloc(bmp_size + 0x36);
+		bmp_ptr = bmp_cache;
+		*(unsigned int*)(bmpHeader+0x02) = bmp_size + 0x36;
+		*(unsigned int*)(bmpHeader+0x12) = BOT_SCREEN_WIDTH;
+		*(unsigned int*)(bmpHeader+0x16) = SCREEN_HEIGHT;
+		*(unsigned int*)(bmpHeader+0x22) = bmp_size;
+		f_write(&myFile, bmpHeader, 0x36, &written);
+		screen_ptr = (void*)BOT_SCREEN;
+		for(int y = 0; y < SCREEN_HEIGHT; y++){
+			for(int x = 0; x < BOT_SCREEN_WIDTH; x++){
+				memcpy(bmp_ptr, screen_ptr[x][y], 3);
+				bmp_ptr += 3;
+			}
+		}
+		f_write(&myFile, bmp_cache, bmp_size + 0x36, &written);
+		f_sync(&myFile);
+		free(bmp_cache);
+		FileClose(&myFile);
+	}
+
 }
 
 void TryScreenShot(){
