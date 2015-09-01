c2a7565df5b42452552522b4cbdffafc85b0eb82
motezazer <motezazer@users.noreply.github.com>
2015-09-01 15:29:39 +0200

    Fixed seed crypto

--- tools/scripts/ncchinfo_gen.py
+++ tools/scripts/ncchinfo_gen.py
@@ -251,7 +251,7 @@ def parseNCCH(fh, offs=0, idx=0, titleId='', standAlone=1):
 	
     if useSeedCrypto:
         keyY = getNewkeyY(keyY,header,struct.pack('I',int(titleId[8:],16))+struct.pack('I',int(titleId[:8],16)))
-        print tab + 'Use Seed NCCH crypto'
+        print tab + 'Uses Seed NCCH crypto'
         print tab + 'Seed KeyY: %s' % hexlify(keyY).upper()
     
     print ''
@@ -265,7 +265,7 @@ def parseNCCH(fh, offs=0, idx=0, titleId='', standAlone=1):
         data = data + parseNCCHSection(header, ncchSection.exefs, 0, 0, 1, tab)
         data = data + genOutName(titleId, ncsdPartitions[idx], b'exefs_norm')
         entries += 1
-        if uses7xCrypto:
+        if uses7xCrypto or useSeedCrypto:
             data = data + parseNCCHSection(header, ncchSection.exefs, uses7xCrypto, useSeedCrypto, 0, tab)
             data = data + genOutName(titleId, ncsdPartitions[idx], b'exefs_7x')
             entries += 1
