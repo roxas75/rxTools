014f017bb93f8225354e42db61aa5dc1b283d8de
AlbertoSONIC <albertosonic9000@gmail.com>
2015-10-22 07:01:59 +0200

    Use cetk to decrypt firmware

--- rxtools/source/features/firm.h
+++ rxtools/source/features/firm.h
@@ -56,7 +56,7 @@ void FirmLoader();
 void rxModeWithSplash(int emu);
 int rxMode(int emu);
 uint8_t* decryptFirmTitleNcch(uint8_t* title, unsigned int size);
-uint8_t* decryptFirmTitle(uint8_t* title, unsigned int size, unsigned int tid, int drive);
+uint8_t *decryptFirmTitle(uint8_t *title, unsigned int size, uint8_t key[16]);
 int applyPatch(void *file, const char *patch);
 
 static inline int getFirmPath(char *s, FirmTid id)
