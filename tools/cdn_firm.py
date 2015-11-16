c4f83596427a176e017aa3d148f03121599d3bd9
capito27 <f.filipe.30.03@gmail.com>
2015-11-16 12:07:22 +0100

    Added N3DS firm to cdn_firm.py and Cetk download
    I also changed a bit the firm file structure for cdn_firm.py

--- tools/cdn_firm.py
+++ tools/cdn_firm.py
@@ -5,17 +5,40 @@ import urllib
 import shutil
 
 firmdir = "firm"
+ctrfirmdir = "O3DS_firm_files"
+ktrfirmdir = "N3DS_firm_files"
 
-def f(title, download):
-	return urllib.urlretrieve("http://nus.cdn.c.shop.nintendowifi.net/ccs/download/%016X/%08X"
-		% (title, download),
-		firmdir + "/%016X.bin" % (title))
+def f(title, download,type):
 
+	if(type=="ctr"):
+		urllib.urlretrieve("http://nus.cdn.c.shop.nintendowifi.net/ccs/download/%016X/cetk"
+			% (title),
+			ctrfirmdir+"/"+firmdir + "/%016X_cetk.bin" % (title))
+		return urllib.urlretrieve("http://nus.cdn.c.shop.nintendowifi.net/ccs/download/%016X/%08X"
+			% (title, download),
+			ctrfirmdir+"/"+firmdir + "/%016X.bin" % (title))
+	else:
+		urllib.urlretrieve("http://nus.cdn.c.shop.nintendowifi.net/ccs/download/%016X/cetk"
+			% (title),
+			ktrfirmdir+"/"+firmdir + "/%016X_cetk.bin" % (title))
+		return urllib.urlretrieve("http://nus.cdn.c.shop.nintendowifi.net/ccs/download/%016X/%08X"
+			% (title, download),
+			ktrfirmdir+ "/"+firmdir + "/%016X.bin" % (title))
+
+	
 print "Downloading 3DS firmware from the CDN..."
-if os.path.isdir(firmdir):
-	shutil.rmtree(firmdir)
-os.mkdir(firmdir)
-f(0x0004013800000002, 0x00000049)
-f(0x0004013800000202, 0x0000000B)
-f(0x0004013800000102, 0x00000016)
+if os.path.isdir(ctrfirmdir):
+	shutil.rmtree(ctrfirmdir)
+if os.path.isdir(ktrfirmdir):
+	shutil.rmtree(ktrfirmdir)
+os.mkdir(ctrfirmdir)
+os.mkdir(ctrfirmdir+"/"+firmdir)
+os.mkdir(ktrfirmdir)
+os.mkdir(ktrfirmdir+"/"+firmdir)
+f(0x0004013800000002, 0x00000049, "ctr")
+f(0x0004013800000102, 0x00000016, "ctr")
+f(0x0004013800000202, 0x0000000B, "ctr")
+#f(0x0004013820000002, 0xXXXXXXXX, "ktr") the relevant file is no longer available on NUS servers, still here for legacy reasons.
+f(0x0004013820000102, 0x00000000, "ktr")
+f(0x0004013820000202, 0x00000000, "ktr")
 print "Downloaded!"
--- tools/readme.txt
+++ tools/readme.txt
@@ -1,7 +1,7 @@
 cdn_firm.exe :
     Use this tool the first time you use rxTools.
-    It will generate 'firm' directory, containing files necessary for the toolkit to work.
-    Once it gets successfully generated, copy it in rxTools directory.
+    It will generate 'firm' directory for both consoles (use only the 'firm' directory for your console type), containing files necessary for the toolkit to work.
+    Once it gets successfully generated, copy the correct 'firm' directory in your rxTools directory (being sd:/rxtools).
 
 msetdg.exe :
     Use this tool if you want to downgrade your System Settings app in your sysNand.
