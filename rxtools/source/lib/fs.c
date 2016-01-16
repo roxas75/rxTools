c2fc46fdf82cc06f240c5df7f11c4147fabb35bf
173210 <root.3.173210@live.com>
2016-01-16 12:35:09 +0900

    rxtools: fs: Initialize NAND when booting

--- rxtools/source/lib/fs.c
+++ rxtools/source/lib/fs.c
@@ -23,7 +23,7 @@ static FATFS fs[3];
 /**Init FileSystems.*/
 bool FSInit(void) {
 	if (f_mount(&fs[0], _T("0:"), 0) != FR_OK) return 0;		//SDCard
-	if (f_mount(&fs[1], _T("1:"), 0) != FR_OK) return 0;		//NAND
+	if (f_mount(&fs[1], _T("1:"), 1) != FR_OK) return 0;		//NAND
 	if (f_mount(&fs[2], _T("2:"), 0) != FR_OK) ; //return 0;	//EmuNAND, Sometimes it doesn't exist
 	return 1;
 }
